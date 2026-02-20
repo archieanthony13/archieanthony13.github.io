aa6593c64d8a3788a6c774aea559a8aacfb0cf41
RetroCoder13 <archieanthony2007@gmail.com>
2026-02-20 13:07:55 +0000

    Showreel cycles through every 5 seconds

--- showreel.js
+++ showreel.js
@@ -3,10 +3,15 @@ const length = showreel.querySelectorAll("div.showreel").length
 
 let image = 0
 
+let showreelTimer = 5000
+let timeout = setTimeout(nextImage,showreelTimer)
+
 function nextImage(){
     image++
     if(image >= length){
         image = 0
     }
     showreel.style.left = "calc(-" + image + "00% - " + image + "0px)"
+
+    timeout = setTimeout(nextImage,showreelTimer)
 }
